c020a043270db4f914cd342f8dfe92546604ec1a
Luke Dudney <luke@kzn.io>
2018-10-16 15:50:33 +0800

    colour coding for guard duty severities

--- index.js
+++ index.js
@@ -384,14 +384,24 @@ var handleGuardDutyFinding = function(event, context) {
 
   var color = "warning";
 
-  if (0 < findingSeverity < 4.0) {
-      color = "#EAEA23";
-  } else if (4.0 < findingSeverity < 7.0) {
-      color = "warning";
-  } else if (7.0 < findingSeverity < 8.9) {
-      color = "danger";
+  severityHigh = [7,8,7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8,7.9,8.0,8.1,8.2,8.3,8.4,8.5,8.6,8.7,8.8,8.9];
+  severityMedium = [4,5,6,4.0,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,6.0,6.1,6.2,6.3,6.4,6.5,6.6,6.7,6.8,6.9]
+  severityLow = [0,1,2,3,0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9]
+
+  if (findingSeverity in severityHigh) {
+    console.log("Guard Duty Severity is high");
+    color = "danger";
+    severity = "High";
+  } else if (findingSeverity in severityMedium) {
+    console.log("Guard Duty Severity is medium");
+    color = "warning";
+    severity = "Medium";
+  } else if (findingSeverity in severityLow) {
+    console.log("Guard Duty Severity is Low");
+    color = "#EAEA23";
+    severity = "Low";
   }
-  
+
   for (var i=0; i< accountIdMap.length; i++) {
     if (accountIdMap[i].accountId === accountId) {
       accountName = accountIdMap[i].name;
@@ -406,7 +416,7 @@ var handleGuardDutyFinding = function(event, context) {
         "color": color,
         "fields": [
           { "title": "Finding Type", "value": findingType, "short": true },
-          { "title": "Severity", "value": findingSeverity, "short": true },
+          { "title": "Severity", "value": findingSeverity + " (" + severity + ")", "short": true },
           { "title": "AWS Account", "value": accountName, "short": true },
           { "title": "AWS Region", "value": ruleRegion, "short": true },
           { "title": "Finding", "value": findingTitle, "short": false },
@@ -560,8 +570,6 @@ exports.handler = function(event, context) {
     processEvent(event, context);
   } else if (config.unencryptedHookUrl) {
     hookUrl = config.unencryptedHookUrl;
-    console.log('posting to ' + hookUrl);
-
     processEvent(event, context);
   } else if (config.kmsEncryptedHookUrl && config.kmsEncryptedHookUrl !== '<kmsEncryptedHookUrl>') {
     var encryptedBuf = new Buffer(config.kmsEncryptedHookUrl, 'base64');
@@ -574,7 +582,6 @@ exports.handler = function(event, context) {
         processEvent(event, context);
       } else {
         hookUrl = "https://" + data.Plaintext.toString('ascii');
-        console.log('posting to ' + data.Plaintext.toString('ascii'));
         processEvent(event, context);
       }
     });
