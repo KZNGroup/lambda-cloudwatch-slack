bc075563ad0f7b426ac6c351f01590c5b23c415a
Luke Dudney <luke@kzn.io>
2018-10-17 12:57:16 +0800

    fixed guard duty severity logic

--- index.js
+++ index.js
@@ -264,12 +264,16 @@ var handleCloudWatch = function(event, context) {
       color = "good";
   }
   
-  for (var i=0; i< accountIdMap.length; i++) {
+  if (accountIdMap) {
+    accountName = _.find(accountIdMap, { accountId: accountId }).name
+  }
+/*  for (var i=0; i< accountIdMap.length; i++) {
     if (accountIdMap[i].accountId === accountId) {
       accountName = accountIdMap[i].name;
       break;
     }
   }
+*/
 
   for (var i=0; i < dimensions.length; i++) {
     dimensionsText += dimensions[i].name + '=' + dimensions[i].value;
@@ -335,11 +339,8 @@ var handleConfigCompliance = function(event, context) {
       color = "good";
   }
   
-  for (var i=0; i< accountIdMap.length; i++) {
-    if (accountIdMap[i].accountId === accountId) {
-      accountName = accountIdMap[i].name;
-      break;
-    }
+  if (accountIdMap) {
+    accountName = _.find(accountIdMap, { accountId: accountId }).name
   }
 
   var slackMessage = {
@@ -383,27 +384,21 @@ var handleGuardDutyFinding = function(event, context) {
   var findingId = message.detail.id;
 
   var color = "warning";
+  var severity = "unknown";
 
-  severityHigh = [7,8,7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8,7.9,8.0,8.1,8.2,8.3,8.4,8.5,8.6,8.7,8.8,8.9];
-  severityMedium = [4,5,6,4.0,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,6.0,6.1,6.2,6.3,6.4,6.5,6.6,6.7,6.8,6.9]
-  severityLow = [0,1,2,3,0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9]
-
-  if (findingSeverity in severityHigh) {
-    color = "danger";
-    severity = "High";
-  } else if (findingSeverity in severityMedium) {
-    color = "warning";
-    severity = "Medium";
-  } else if (findingSeverity in severityLow) {
+  if (findingSeverity >= 0.0 && findingSeverity <= 3.9) {
     color = "#EAEA23";
     severity = "Low";
+  } else if (findingSeverity >= 4.0 && findingSeverity <= 6.9) {
+    color = "warning";
+    severity = "Medium";
+  } else if (findingSeverity >= 7.0 && findingSeverity <= 8.9) {
+    color = "danger";
+    severity = "High";
   }
 
-  for (var i=0; i< accountIdMap.length; i++) {
-    if (accountIdMap[i].accountId === accountId) {
-      accountName = accountIdMap[i].name;
-      break;
-    }
+  if (accountIdMap) {
+    accountName = _.find(accountIdMap, { accountId: accountId }).name
   }
 
   var slackMessage = {
